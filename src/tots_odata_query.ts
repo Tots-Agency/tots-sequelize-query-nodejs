d257d2688d9971926bdf8f24c88603020bcec766
Matias Camiletti <matias.camiletti@gmail.com>
2026-01-10 08:39:50 -0300

    feat: add dto process and options entity.

--- src/tots_odata_query.ts
+++ src/tots_odata_query.ts
@@ -1,5 +1,14 @@
 import parseOData = require("odata-sequelize");
 
+export interface TotsOdataOptions {
+    filters: string;
+    top: number;
+    skip: number;
+    orderBy: string;
+    include?: any;
+    dto?: any;
+}
+
 export class TotsOdataQuery {
 
     constructor(protected sequelize: any, protected model: any) { }
@@ -14,15 +23,15 @@ export class TotsOdataQuery {
      */
     async paginateWithInclude(include: any, filters: string, top: number, skip: number, orderBy: string = '') {
         let odataString = '';
-        if(filters != '' && filters.length > 0){
+        if (filters != '' && filters.length > 0) {
             odataString = filters;
         }
-        if(orderBy != '' && orderBy.length > 0){
+        if (orderBy != '' && orderBy.length > 0) {
             odataString += '&$orderby=' + orderBy;
         }
 
         let query: any;
-        if(odataString != '' && odataString.length > 0){
+        if (odataString != '' && odataString.length > 0) {
             query = parseOData(TotsOdataQuery.processFilters(odataString), this.sequelize);
         }
 
@@ -53,19 +62,60 @@ export class TotsOdataQuery {
         return this.paginateWithInclude(undefined, filters, top, skip, orderBy);
     }
 
+    async exec(filters: string, top: number, skip: number, orderBy: string = '', options: TotsOdataOptions) {
+        return this.execWithOptions({
+            filters: filters,
+            top: top,
+            skip: skip,
+            orderBy: orderBy,
+            include: options.include,
+            dto: options.dto
+        });
+    }
+
+    async execWithOptions(options: TotsOdataOptions) {
+        let odataString = '';
+        if (options.filters != '' && options.filters.length > 0) {
+            odataString = options.filters;
+        }
+        if (options.orderBy != '' && options.orderBy.length > 0) {
+            odataString += '&$orderby=' + options.orderBy;
+        }
+
+        let query: any;
+        if (odataString != '' && odataString.length > 0) {
+            query = parseOData(TotsOdataQuery.processFilters(odataString), this.sequelize);
+        }
+
+        const { count, rows } = await this.model.findAndCountAll({
+            include: options.include,
+            where: query?.where ?? null,
+            order: query?.order ?? null,
+            offset: options.skip,
+            limit: options.top,
+        });
+
+        return {
+            current_page: (options.skip / options.top) + 1,
+            data: options.dto != undefined ? rows.map((row: any) => options.dto.fromModel(row)) : rows,
+            per_page: options.top,
+            total: count
+        };
+    }
+
     static prependFilters(prepend: string, filters: string): string {
         let result = '$filter=' + (prepend.replace('$filter=', ''));
         let processedFilters = filters.replace('$filter=', '');
-        if(processedFilters.length > 0) {
-          result += ' and (' + processedFilters + ')';
+        if (processedFilters.length > 0) {
+            result += ' and (' + processedFilters + ')';
         }
         return result;
     }
 
     static processFilters(filters: string): string {
         // Verify if start with $filter
-        if(filters.startsWith('$filter=')) {
-          return filters;
+        if (filters.startsWith('$filter=')) {
+            return filters;
         }
         return '$filter=' + filters;
     }
