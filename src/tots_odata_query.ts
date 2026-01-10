4adfd164ca5411800685611ab4d317bb0f795aab
Matias Camiletti <matias.camiletti@gmail.com>
2026-01-10 08:52:30 -0300

    feat: add optional variables

--- src/tots_odata_query.ts
+++ src/tots_odata_query.ts
@@ -1,10 +1,10 @@
 import parseOData = require("odata-sequelize");
 
 export interface TotsOdataOptions {
-    filters: string;
-    top: number;
-    skip: number;
-    orderBy: string;
+    filters?: string;
+    top?: number;
+    skip?: number;
+    orderBy?: string;
     include?: any;
     dto?: any;
 }
@@ -62,24 +62,24 @@ export class TotsOdataQuery {
         return this.paginateWithInclude(undefined, filters, top, skip, orderBy);
     }
 
-    async exec(filters: string, top: number, skip: number, orderBy: string = '', options: TotsOdataOptions) {
+    async exec(filters: string, top: number, skip: number, orderBy: string = '', options?: TotsOdataOptions) {
         return this.execWithOptions({
             filters: filters,
             top: top,
             skip: skip,
             orderBy: orderBy,
-            include: options.include,
-            dto: options.dto
+            include: options?.include,
+            dto: options?.dto
         });
     }
 
     async execWithOptions(options: TotsOdataOptions) {
         let odataString = '';
-        if (options.filters != '' && options.filters.length > 0) {
-            odataString = options.filters;
+        if (options?.filters != '' && options?.filters.length > 0) {
+            odataString = options?.filters;
         }
-        if (options.orderBy != '' && options.orderBy.length > 0) {
-            odataString += '&$orderby=' + options.orderBy;
+        if (options?.orderBy != '' && options?.orderBy.length > 0) {
+            odataString += '&$orderby=' + options?.orderBy;
         }
 
         let query: any;
@@ -88,17 +88,17 @@ export class TotsOdataQuery {
         }
 
         const { count, rows } = await this.model.findAndCountAll({
-            include: options.include,
+            include: options?.include,
             where: query?.where ?? null,
             order: query?.order ?? null,
-            offset: options.skip,
-            limit: options.top,
+            offset: options?.skip,
+            limit: options?.top,
         });
 
         return {
-            current_page: (options.skip / options.top) + 1,
-            data: options.dto != undefined ? rows.map((row: any) => options.dto.fromModel(row)) : rows,
-            per_page: options.top,
+            current_page: (options?.skip / options?.top) + 1,
+            data: options?.dto != undefined ? rows.map((row: any) => options?.dto.fromModel(row)) : rows,
+            per_page: options?.top,
             total: count
         };
     }
